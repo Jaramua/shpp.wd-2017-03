5cdf052a0d11436a909f7b589ea33f5e1912176e
Yuliia Kasianova <jaramua@gmail.com>
2017-03-26 17:35:47 +0300

    Add files via upload

--- pset3/landing/script.js
+++ pset3/landing/script.js
@@ -1,23 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-	$(function() {
-		$(window).scroll(function() {
-			if($(this).scrollTop() != 0) {
-				$('#toTop').fadeIn();
-			} else {
-				$('#toTop').fadeOut();
-			}
-		}); 
-		$('#toTop').click(function() {
-			$('body,html').animate({scrollTop:0},800);
-		});
-	});
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() != 0) {
+                $('#toTop').fadeIn();
+            } else {
+                $('#toTop').fadeOut();
+            }
+        });
+        $('#toTop').click(function () {
+            $('body,html').animate({scrollTop: 0}, 800);
+        });
+    });
 
-	$("#menu").on("click","a", function (event) {
-		event.preventDefault();
-		var id  = $(this).attr('href'),
-		top = $(id).offset().top - ($(window).height() - $(id).outerHeight(true)) / 2;
-		$('body,html').animate({scrollTop: top}, 300);
-	});
+    $("#menu").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top - ($(window).height() - $(id).outerHeight(true)) / 2;
+        $('body,html').animate({scrollTop: top}, 300);
+    });
 
 });
