d87cba5835edb733e146921691d55b49b0acbee2
Yuliia Kasianova <jaramua@gmail.com>
2017-04-06 20:18:55 +0300

    add pset 4

---
+++ ps4/task2/main.php
@@ -0,0 +1,43 @@
+<?php 
+
+session_start();
+
+$race = "";
+$err = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (empty($_POST["race"])) {
+		$err = "Choose your race or die!!!!";
+		$_SESSION['error'] = $err;
+		header('Location: index.php');
+	} else {
+		$race = ($_POST["race"]);
+	}
+	
+
+	$arr = array();
+	$jsonFile = "data.json";
+
+	$data = file_get_contents($jsonFile);
+	$arr = json_decode($data, true);
+
+	if (isset($arr[$race])) {
+		$arr[$race]++;
+	} else {
+		$arr[$race] = 1;
+	}
+
+	$data = json_encode($arr);
+	file_put_contents($jsonFile, $data);
+
+	header('Content-Type: application/json');
+	echo $_SESSION[$data];
+
+	if (isset($_POST['submit'])) {
+		if (!empty($_POST["race"])) {
+			header('Location: output.php');
+		}
+		
+	}
+}
+?>
---
+++ ps4/task2/output.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Statictics</title>
+	<style>
+	</style>
+</head>
+
+<body style="height: auto">
+	<div id="chartDiv"></div>
+
+	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	<script type="text/javascript">
+		google.charts.load('current', {'packages':['corechart']});
+		google.charts.setOnLoadCallback(drawChart);
+
+		function drawChart() {
+			$.getJSON("data.json",
+				function(data){
+					var i;
+					var arr = [];
+					for (i in data) {
+						arr.push([i, data[i]]);
+					}
+					output(arr);
+				});
+
+			function output(info) {
+				var data = new google.visualization.DataTable();
+				data.addColumn('string', 'Race');
+				data.addColumn('number', 'Votes');
+				data.addRows(info);
+				var options = {
+					title: 'Race popularity',
+					width: 1100,
+					height: 800
+				};
+				var chart = new google.visualization.PieChart(document.getElementById('chartDiv'));
+				chart.draw(data, options);
+			}
+		}
+	</script>
+
+</body>
+</html>
